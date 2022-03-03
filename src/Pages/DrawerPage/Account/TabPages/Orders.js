552ab452713c92fcd337bb7ea1f5be12f851e20d
Usama <usama.amjad32@gmail.com>
2022-03-04 01:40:45 +0500

    Update order

--- src/Pages/DrawerPage/Account/TabPages/Orders.js
+++ src/Pages/DrawerPage/Account/TabPages/Orders.js
@@ -5,6 +5,8 @@ import Box from "../../../../UI/Layout/Box";
 import Grid from "../../../../UI/Layout/Grid";
 import data from "../../../../Constants/dummy_orders.json";
 import Search from "../../../../UI/SearchField/Search";
+import Card from "../../../../UI/Card/Card";
+import Tabs from "./../../../../UI/Tabs/Tabs";
 
 const orderCategories = [
   {
@@ -19,11 +21,16 @@ const orderCategories = [
 
 const Orders = () => {
   return (
-    <Grid container>
+    // <Card>
+    <Grid container spacing={3}>
       <Grid item xs={12}>
         <Search searchOptions={data} categories={orderCategories} />
       </Grid>
+      {/* <Grid item xs={12}>
+          <Tabs></Tabs>
+        </Grid> */}
     </Grid>
+    // </Card>
   );
 };
 
