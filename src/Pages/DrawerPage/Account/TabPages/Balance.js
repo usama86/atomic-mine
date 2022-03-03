3123060e897333295c20e5028da894a53b5cc799
Usama <usama.amjad32@gmail.com>
2022-03-03 15:59:25 +0500

    s
    s

--- src/Pages/DrawerPage/Account/TabPages/Balance.js
+++ src/Pages/DrawerPage/Account/TabPages/Balance.js
@@ -1,8 +1,42 @@
 import LabelChild from "./../../../../UI/LabelChild";
-
+import Stack from "./../../../../UI/Layout/Stack";
+import Grid from "./../../../../UI/Layout/Grid";
+import Card from "./../../../../UI/Card/Card";
 const Balance = () => {
   //9
-  return <LabelChild />;
+  return (
+    <Card>
+      <Grid spacing={6} container>
+        <Grid item container xsSize={6}>
+          <LabelChild label={"1st"}>Hi</LabelChild>
+        </Grid>
+        <Grid item container xsSize={6}>
+          <LabelChild label={"2st"}>Hi</LabelChild>
+        </Grid>
+        <Grid item container xsSize={6}>
+          <LabelChild label={"3st"}>Hi</LabelChild>
+        </Grid>
+        <Grid item container xsSize={6}>
+          <LabelChild label={"4st"}>Hi</LabelChild>
+        </Grid>
+        <Grid item container xsSize={6}>
+          <LabelChild label={"5st"}>Hi</LabelChild>
+        </Grid>
+        <Grid item container xsSize={6}>
+          <LabelChild label={"6st"}>Hi</LabelChild>
+        </Grid>
+        <Grid item container xsSize={6}>
+          <LabelChild label={"7st"}>Hi</LabelChild>
+        </Grid>
+        <Grid item container xsSize={6}>
+          <LabelChild label={"8st"}>Hi</LabelChild>
+        </Grid>
+        <Grid item container xsSize={6}>
+          <LabelChild label={"9st"}>Hi</LabelChild>
+        </Grid>
+      </Grid>
+    </Card>
+  );
 };
 
 export default Balance;
--- src/UI/LabelChild.js
+++ src/UI/LabelChild.js
@@ -4,14 +4,14 @@ import Grid from "./Layout/Grid";
 
 const LabelChild = ({ label, children }) => {
   return (
-    <Grid container>
+    <>
       <Grid item xsSize={4}>
         <Typography>{label}</Typography>
       </Grid>
       <Grid item xsSize={4}>
-        Hello
+        {children}
       </Grid>
-    </Grid>
+    </>
   );
 };
 
