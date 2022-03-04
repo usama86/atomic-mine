a3c2598ad98294045a7b873f8a300d61b45f5a83
bhattiasad99 <bhatti.asad99@gmail.com>
2022-03-04 18:53:48 +0500

    .

--- src/Pages/DrawerPage/Account/TabPages/RiskManagementModals/BlacklistOption.js
+++ src/Pages/DrawerPage/Account/TabPages/RiskManagementModals/BlacklistOption.js
@@ -1,11 +1,5 @@
 import React from "react";
-import Switch from "../../../../../UI/Switch/Switch";
-
-import Typography from "../../../../../UI/Typography/Typography";
-import TextFieldComp from "../../../../../UI/TextField/TextFieldComp";
 import Stack from "../../../../../UI/Layout/Stack";
-import Box from "../../../../../UI/Layout/Box";
-import Button from "../../../../../UI/Button/Button";
 
 const BlacklistOption = () => {
   return <Stack>BlacklistOption</Stack>;
