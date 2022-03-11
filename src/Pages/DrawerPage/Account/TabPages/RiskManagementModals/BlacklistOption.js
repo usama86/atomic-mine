6bca1ce988a8685cfb5fc3cf0f0dd9daab6d0883
Usama <usama.amjad32@gmail.com>
2022-03-12 00:53:24 +0500

    Added Blacklist

--- src/Pages/DrawerPage/Account/TabPages/RiskManagementModals/BlacklistOption.js
+++ src/Pages/DrawerPage/Account/TabPages/RiskManagementModals/BlacklistOption.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Stack from "../../../../../UI/Layout/Stack";
-import Search from "./../../../../../UI/Table/TableSearch";
+import Search from "./../../../../../UI/AutoComplete/Autocomplete";
 import Checkbox from "./../../../../../UI/Checkbox/Checkbox";
 
 const BlacklistOption = ({ triggerClose }) => {
   return (
     <Stack>
-      <Search />
+      <Search Label="Blacklist" />
       <Checkbox Label="No Trade" />
       <Checkbox Label="Liq Only" />
     </Stack>
---
+++ src/UI/AutoComplete/Autocomplete.js
@@ -0,0 +1,45 @@
+import * as React from "react";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+
+export default function ComboBox({ Label }) {
+  return (
+    <Autocomplete
+      disablePortal
+      options={top100Films}
+      sx={{ width: 300 }}
+      renderInput={(params) => <TextField {...params} label={Label} />}
+    />
+  );
+}
+
+// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
+const top100Films = [
+  { label: "The Shawshank Redemption", year: 1994 },
+  { label: "The Godfather", year: 1972 },
+  { label: "The Godfather: Part II", year: 1974 },
+  { label: "The Dark Knight", year: 2008 },
+  { label: "12 Angry Men", year: 1957 },
+  { label: "Schindler's List", year: 1993 },
+  { label: "Pulp Fiction", year: 1994 },
+  {
+    label: "The Lord of the Rings: The Return of the King",
+    year: 2003,
+  },
+  { label: "The Good, the Bad and the Ugly", year: 1966 },
+  { label: "Fight Club", year: 1999 },
+  {
+    label: "The Lord of the Rings: The Fellowship of the Ring",
+    year: 2001,
+  },
+  {
+    label: "Star Wars: Episode V - The Empire Strikes Back",
+    year: 1980,
+  },
+  { label: "Forrest Gump", year: 1994 },
+  { label: "Inception", year: 2010 },
+  {
+    label: "The Lord of the Rings: The Two Towers",
+    year: 2002,
+  },
+];
